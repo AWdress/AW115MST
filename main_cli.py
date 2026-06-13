497f1313f52d14d9492a80d0f46a8935dfa2b17c
AWdress <awdress@users.noreply.github.com>
2026-06-13 23:17:14 +0800

    chore: 版本号更新至 1.2.6

--- main_cli.py
+++ main_cli.py
@@ -150,7 +150,7 @@ def main():
     parser.add_argument(
         '-v', '--version',
         action='version',
-        version='AW115MST v1.2.5'
+        version='AW115MST v1.2.6'
     )
     
     args = parser.parse_args()
